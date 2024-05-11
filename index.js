23cd9817028eb084fa6058d44f258eff2af26398
fftyghu <js1lalacurr@hotmail.com>
2024-05-11 07:03:41 +0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const isPrime = (num) => {
-  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
-    if (num % i === 0) return false;
+function longestPalindrome(s) {
+  const map = new Map();
+  let hasOdd = false;
+  let result = 0;
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
   }
-  return num > 1;
-};
+  for (const count of map.values()) {
+    if (count % 2 === 0) result += count;
+    else {
+      result += count - 1;
+      hasOdd = true;
+    }
+  }
+  return hasOdd ? result + 1 : result;
+}
